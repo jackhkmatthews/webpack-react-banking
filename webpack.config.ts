7d358f8227d2fe05a7d144063ec1816eeec3ce03
Jack Matthews <jackhkmatthews@gmail.com>
2021-04-25 11:48:08 +0100

    feat(webpack): configure with dev server

---
+++ webpack.config.ts
@@ -0,0 +1,67 @@
+import CopyPlugin from "copy-webpack-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import * as path from "path";
+import * as webpack from "webpack";
+import * as devsever from "webpack-dev-server";
+
+interface Configuration extends webpack.Configuration {
+  devServer?: devsever.Configuration;
+}
+
+const config: Configuration = {
+  mode: "development",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-typescript"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devtool: "inline-source-map",
+  output: {
+    // can use [name] in file name to get entry name if
+    // entry option is an object
+    filename: "static/bundle.js",
+    path: path.resolve(__dirname, "dist/"),
+    // tells bundled files where to request other bundles from
+    publicPath: "/",
+    clean: true,
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "public/"),
+          to: path.resolve(__dirname, "dist"),
+          filter: (resourcePath) => !resourcePath.includes("index.html"),
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      inject: "body",
+      title: "Webpack, React and Typescript",
+      filename: "index.html",
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    contentBase: path.resolve("public"),
+    hot: true,
+    port: 3000,
+    watchContentBase: true,
+  },
+};
+
+export default config;
