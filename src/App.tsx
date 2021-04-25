41b7b36b072f11d94a40d74d8ca0ec654bdece5d
Jack Matthews <jackhkmatthews@gmail.com>
2021-04-25 13:19:07 +0100

    feat(App): pass account data to account card

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,27 @@ import { hot } from "react-hot-loader/root";
 import React from "react";
 
 import { Page } from "./components/Page/Page.styled";
-import { AccountProvider } from "./types/mockly";
+import { MocklyFetchState } from "./types/mockly";
 import * as S from "./App.styled";
-import { useFetch } from "./hooks/useFetch";
-
-const mockProvider: AccountProvider = {
-  title: "Monzo",
-  account_number: "12345678",
-  sort_code: "12-34-56",
-  description: "Current Account",
-};
+import { FetchStatus, useFetch } from "./hooks/useFetch";
 
 const App = () => {
-  const fetchState = useFetch(
+  const {
+    data: { provider },
+    status,
+  }: MocklyFetchState = useFetch(
     "http://www.mocky.io/v2/5c62e7c33000004a00019b05"
   );
-  console.log(fetchState);
   return (
     <Page>
-      <S.TopAccountCard
-        title={mockProvider.title}
-        accountNumber={mockProvider.account_number}
-        type={mockProvider.description}
-        sortCode={mockProvider.sort_code}
-      />
+      {status === FetchStatus.FETCHED && (
+        <S.TopAccountCard
+          title={provider.title}
+          accountNumber={provider.account_number}
+          type={provider.description}
+          sortCode={provider.sort_code}
+        />
+      )}
     </Page>
   );
 };
--- src/types/mockly.ts
+++ src/types/mockly.ts
@@ -1,6 +1,19 @@
+import { FetchState } from "../hooks/useFetch";
+
 export interface AccountProvider {
   title: string;
   account_number: string;
   sort_code: string;
   description: string;
+}
+
+export interface MocklyAccount {
+  id: any;
+  provider: AccountProvider;
+  balance: any;
+  transations: any[];
+}
+
+export interface MocklyFetchState extends FetchState {
+  data: MocklyAccount;
 }
