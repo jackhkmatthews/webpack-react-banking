cbefc020a1b6e488ecf091efd153cef0976ec4d3
Jack Matthews <jackhkmatthews@gmail.com>
2021-04-25 12:25:13 +0100

    feat(styles): normalise styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Normalize } from "styled-normalize";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const Root = () => (
+  <>
+    <Normalize />
+    <App />
+  </>
+);
+
+ReactDOM.render(<Root />, document.getElementById("root"));
